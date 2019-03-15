e8dc6fc2738db57afa9636f70c6afab3ec732ce8
Robert Hernandez <rahernande96@gmail.com>
2019-03-15 06:35:37 -0400

    correcion de mails de estado pendinte

--- resources/views/mails/user_pending.blade.php
+++ resources/views/mails/user_pending.blade.php
@@ -4,7 +4,7 @@
 
 <p>Hola {{ session('name') }}</p>
 <p>Tu inscripción al evento {{ $event->name }} esta siendo procesada (pago pendiente)</p>
-<p>Los siguientes datos podrán ser pedidos para verificar la inscripción:</p>
+<p>Si el pago es exitoso los siguientes datos podrán ser pedidos para verificar la inscripción:</p>
 <p>{{ session('name') }}</p>
 <p>{{ session('email') }}</p>
 <p>Monto de la transacción:{{ $event->price }}</p>
