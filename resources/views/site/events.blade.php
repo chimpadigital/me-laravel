cd6302d650e21c096eae6f65b715c5fcddbfd4ef
chimpadigital <sdesigncba@gmail.com>
2019-03-28 13:58:04 -0300

    arreglo animación circulo

--- resources/views/site/events.blade.php
+++ resources/views/site/events.blade.php
@@ -75,7 +75,7 @@
             </div>
         </div>
         <!-- Welcome thumb -->
-        <div class="welcome-thumb wow fadeInDown" data-wow-delay="0.4s">
+        <div class="welcome-thumb">
             <img src="{{ asset('img/ilustraciones/fondo-circulo.svg') }}" alt="">
         </div>
     </section>
